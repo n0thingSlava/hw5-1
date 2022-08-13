520f93e5de08acf0272f6d69deeb0a5acaa6421d
Slav1k <maznevslava@yandex.ru>
2022-08-13 11:40:43 +0500

    dz

--- src/Main.java
+++ src/Main.java
@@ -9,42 +9,49 @@ public class Main {
         int[] abc = {77, 88, 99};
         //задание №2
         for (int i = 0; i < light.length; i++) {
-            if (i == light.length)
-                System.out.println(light[i]);
+            if (i == light.length - 1)
+                System.out.print(light[i]);
             else System.out.print(light[i] + ",");
         }
         System.out.println();
 
         for (int q = 0; q < doub.length; q++) {
-            if (q == doub.length)
-                System.out.println(doub[q]);
+            if (q == doub.length - 1)
+                System.out.print(doub[q]);
             else System.out.print(doub[q] + ",");
         }
         System.out.println();
 
         for (int p = 0; p < abc.length; p++) {
-            if (p == abc.length)
-                System.out.println(abc[p]);
+            if (p == abc.length - 1)
+                System.out.print(abc[p]);
             else System.out.print(abc[p] + ",");
         }
         System.out.println();
 
         //Задача №3
-        for (int i = light.length - 1; i >= 0; i--) {
-            System.out.print(light[i] + ",");}
+            for (int i = light.length - 1;i>=0; i--) {
+                System.out.print(light[i] + ",");
+        System.out.println();
+            }
+            for (int q = doub.length - 1; q >= 0; q--) {
+                System.out.print(doub[q] + ",");
+            }
         System.out.println();
 
-        for (int q = doub.length -1 ; q>=0; q--) {
-            System.out.print(doub[q]+ ",");}
-        System.out.println();
-
-        for (int p = abc.length - 1; p >= 0; p--) {
-            System.out.print(abc[p] + ",");}
+            for (int p = abc.length - 1; p >= 0; p--) {
+                System.out.print(abc[p] + ",");
+            }
         System.out.println();
 //Задача №4
-        for(int i = 0;i < light.length; i++ )
-            if(light[i] % 2 == 0)
-                System.out.print("Сделал только четные числа " + light[i] + ",");
-    }
+        for (int i = 0, j = 1, result = light[0] + 1, result1 = light[2] + 1; i < light.length; i++)
+            if (light[i] % 2 == 0)
+               if (light[i] % 2 != 0)
+                  System.out.print("Сделал четные " + light[i] + ",");
+               else
+                  System.out.println("к нечетним прибавил +1 получил = " + result + " и " + result1);
+
+        }
 
     }
+
